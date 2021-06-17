c29375d99a7585b9699da05b5d57362286a56011
立岩斉明 <n-tateiwa@math.kyushu-u.ac.jp>
2021-06-17 15:23:14 +0900

    introduce argparse

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include <sstream>
-#include <deque>
-#include <vector>
-#include <algorithm>
-#include <random>
+#include <argparse/argparse.hpp>
 
 #include "def.h"
 #include "utils.h"
@@ -11,8 +7,44 @@
 #include "trial.h"
 
 
-int main()
+int main(
+      int argc,
+      char *argv[]
+      )
 {
+   argparse::ArgumentParser program("master mind");
+
+   program.add_argument("num_colors")
+      .help("number of colors")
+      .action([](const std::string& value) { return std::stoi(value); });
+
+   program.add_argument("num_pins")
+      .help("number of pins")
+      .action([](const std::string& value) { return std::stoi(value); });
+
+   program.add_argument("--no_duplicate")
+      .help("secret codes donot have color duplication")
+      .default_value(false)
+      .implicit_value(true);
+   try
+   {
+      program.parse_args(argc, argv);
+   }
+   catch (const std::runtime_error& err)
+   {
+      std::cout << err.what() << std::endl;
+      std::cout << program;
+      exit(0);
+   }
+
+   int nColors = program.get<int>("num_colors");
+   int nPins = program.get<int>("num_pins");
+   bool duplicate = !program.get<bool>("--no_duplicate");
+
+   // create config object
+   Config config(nColors, nPins, duplicate);
+   exit(0);
+
    // とりあえず(2色, 2ピン)
    CodeList S{ Code{0,0}, Code{0,1}, Code{1,0}, Code{1,1} };
 
--- src/utils.h
+++ src/utils.h
@@ -29,6 +29,49 @@ std::string strCode(
 
 
 /**
+ * パラメータ管理
+ */
+class Config
+{
+   public:
+      int nColors;      // number of colors
+      int nPins;        // number of pins
+      bool duplicate;   // if it is true, then color-duplication is allowed
+
+      /**
+      * @brief Constructor
+      * @param[in] inNColors 色数
+      * @param[in] inNPins ピン数
+      * @param[in] inDuplicate 色の重複を許すかどうか
+      */
+      Config(
+            int inNColors,
+            int inNPins,
+            bool inDuplicate
+            )
+         :
+            nColors(inNColors),
+            nPins(inNPins),
+            duplicate(inDuplicate)
+      {}
+
+      /**
+      * @brief 管理情報をstringにする
+      * @return std::string
+      */
+      std::string str()
+      {
+         std::ostringstream os;
+         os << "Config:" << std::endl;
+         os << "  nColors: " << nColors << std::endl;
+         os << "  nPins: " << nPins << std::endl;
+         os << "  duplicate: " << duplicate << std::endl;
+         return os.str();
+      }
+};
+
+
+/**
 * @fn HitBlow countHitBlow(Code &code, Code &guess)
 * @brief コード2つから, hit, blowを計算する
 * @param[in] code コード
