16ec19d7d9c7e0afa789e1bdfe9a23d3817535f0
立岩斉明 <n-tateiwa@math.kyushu-u.ac.jp>
2021-06-20 12:25:58 +0900

    add setGuessCandidates

--- src/codeGenerator.h
+++ src/codeGenerator.h
@@ -2,6 +2,7 @@
 #define __CODE_GENERATOR_H__
 
 #include <cassert>
+#include <set>
 
 #include "def.h"
 #include "utils.h"
@@ -31,6 +32,83 @@ void allCodeGenerator(
    else
    {
       itertools.permutations(colors, config.nPins, codePtrList);
+   }
+}
+
+
+/**
+ * @fn void setGuessCandidates(CodePtrList &codePtrList, CodeList &guessHist,
+ *             Config &config,CodePtrList &guessCandidates)
+ * @brief 推測コード集合を計算する
+ * @param[in] codePtrList コード集合
+ * @param[in] guessHist 推測履歴
+ * @param[in] config パラメタ
+ * @param[out] guessCandidates 推測コード集合
+ * @details まだ推測に使用されていない色についての区別は行わない
+ */
+void setGuessCandidates(
+      CodePtrList &codePtrList,
+      CodeList &guessHist,
+      Config &config,
+      CodePtrList &guessCandidates
+      )
+{
+   assert ( guessCandidates.size() == 0 );
+
+   // get colors used in guesses
+   std::set<ColorType> histColors;
+   for ( auto guess : guessHist )
+   {
+      for ( auto color : guess )
+      {
+         histColors.insert(color);
+      }
+   }
+
+   if ( histColors.size() >= config.nColors - 1 )
+   {
+      guessCandidates = codePtrList;
+      return;
+   }
+
+   Itertools itertools;
+   auto colors = itertools.range<ColorType>(0, config.nColors);
+   ColorType minColorNotInHist;
+   for ( auto color : colors )
+   {
+      if ( histColors.find(color) == histColors.end() )
+      {
+         minColorNotInHist = color;
+         break;
+      }
+   }
+
+   // extract guess code
+   for ( auto code : codePtrList )
+   {
+      int tmp = -1;
+      bool insert = true;
+      for ( auto color : *code )
+      {
+         if ( histColors.find(color) == histColors.end() )
+         {
+            if ( tmp == -1 && color != minColorNotInHist )
+            {
+               insert = false;
+               break;
+            }
+            else if ( color < tmp )
+            {
+               insert = false;
+               break;
+            }
+            tmp = color;
+         }
+      }
+      if ( insert )
+      {
+         guessCandidates.push_back(code);
+      }
    }
 }
 
