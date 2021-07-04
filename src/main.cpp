0f26dbc5614995ddd6b6bffafb0813916eada42c
立岩斉明 <n-tateiwa@math.kyushu-u.ac.jp>
2021-07-04 21:03:38 +0900

    introduce mpi

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 #include "run.h"
+#ifdef USE_MPI
+#include "mpi.h"
+#include "runMpi.h"
+#endif
 
 
 int main(
@@ -8,6 +12,13 @@ int main(
       char *argv[]
       )
 {
+#ifdef USE_MPI
+   int size, rank;
+   MPI_Init( &argc, &argv );
+   MPI_Comm_size( MPI_COMM_WORLD, &size );
+   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+#endif
+
    namespace mm = MasterMind;
    mm::Config config = mm::getConfig(argc, argv);
    std::cout << config.str() << std::endl;
@@ -16,10 +27,19 @@ int main(
    {
       mm::runInteractive(config);
    }
+#ifdef USE_MPI
+   else if ( size > 0 )
+   {
+      mm::runTestPara(config, size, rank);
+   }
+#endif
    else
    {
       mm::runTest(config);
    }
 
+#ifdef USE_MPI
+   MPI_Finalize();
+#endif
    return 0;
 }
---
+++ src/runMpi.cpp
@@ -0,0 +1,85 @@
+#include <omp.h>
+#include "mpi.h"
+#include "runMpi.h"
+
+
+namespace MasterMind
+{
+
+void runTestPara(
+      Config &config,
+      int nProcesses,
+      int rank
+      ) noexcept
+{
+   CodePtrList S;
+   allCodeGenerator(config, S);
+
+   std::vector<int> countTable(S.size());
+   std::vector<double> timeTable(S.size());
+   auto startTotal = omp_get_wtime();
+   int sendcount = 0;
+
+   for ( int i = 0; i*nProcesses + rank < static_cast<int>(S.size()); ++i )
+   {
+      // test code
+      auto secret = S[i*nProcesses + rank];
+      sendcount++;
+      config.setSecret(*secret);
+      CodePtrList testS = copy(S);
+      CodeList guessHist;
+      CodePtrList G;
+
+      int count = 0;
+      auto start = omp_get_wtime();
+      while( testS.size() > 1 )
+      {
+         count++;
+         G.clear();
+         setGuessCandidates(S, guessHist, config, G);
+         auto guess = policy(testS, G, config);
+         trial(testS, guess, config);
+         guessHist.push_back(guess);
+      }
+      auto end = omp_get_wtime();
+
+      assert( *testS[0] == *secret );
+      double elapsed = end - start;
+      countTable[i] = count;
+      timeTable[i] = elapsed;
+   }
+
+
+   if( nProcesses > 0 )
+   {
+      MPI_Gather(countTable.data(), sendcount, MPI_INT, countTable.data(), sendcount, MPI_INT, 0, MPI_COMM_WORLD);
+      MPI_Gather(timeTable.data(), sendcount, MPI_DOUBLE, timeTable.data(), sendcount, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+   }
+
+   if( rank == 0 )
+   {
+      auto endTotal = omp_get_wtime();
+      // output statistics
+      auto getMax = [&](auto &v){ return *std::max_element(v.cbegin(), v.cend()); };
+      auto getSum = [&](auto &v){ return std::accumulate(v.cbegin(), v.cend(), 0.0); };
+      auto getAve = [&](auto &v){ return getSum(v) / v.size(); };
+      int maxCount         = getMax(countTable);
+      double averageCount  = getAve(countTable);
+      double totalTime     = endTotal - startTotal;
+      double averageTime   = getAve(timeTable);
+      std::cout
+         << std::endl
+         << "Log,num colors,num pins,policy,max count,average count,total time,average time"
+         << std::endl;
+      std::cout
+         << "log"
+         << "," << config.nColors   << "," << config.nPins
+         << "," << config.policy
+         << "," << maxCount         << "," << averageCount
+         << "," << totalTime        << "," << averageTime
+         << std::endl;
+   }
+}
+
+}; // MasterMind
+
